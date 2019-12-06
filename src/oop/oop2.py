98215e639de5bfc51645e22eede15069ebca0621
johnschneider1 <johnlawschneider@gmail.com>
2019-12-06 12:57:41 -0600

    finished product

--- src/oop/oop2.py
+++ src/oop/oop2.py
@@ -3,9 +3,13 @@
 # Also change it so the num_wheels defaults to 4 if not specified when the
 # object is constructed.
 
+
 class GroundVehicle():
-    def __init__(self, num_wheels):
+    def __init__(self, num_wheels=4):
         self.num_wheels = num_wheels
+
+    def drive(self):
+        return f"vroooom"
 
     # TODO
 
@@ -19,6 +23,15 @@ class GroundVehicle():
 
 # TODO
 
+class Motorcycle(GroundVehicle):
+    def __init__(self, num_wheels=2):
+        super().__init__(self)
+        self.num_wheels = num_wheels
+
+    def drive(self):
+        return f"BRAAAP!!"
+
+
 vehicles = [
     GroundVehicle(),
     GroundVehicle(),
@@ -28,5 +41,8 @@ vehicles = [
 ]
 
 # Go through the vehicles list and print the result of calling drive() on each.
+
+for vehicle in vehicles:
+    print(vehicle.drive())
 
 # TODO
